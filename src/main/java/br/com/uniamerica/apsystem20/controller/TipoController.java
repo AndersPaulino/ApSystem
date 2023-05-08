b81009182e3c67d5099fc3b7f0593654ad514fc6
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2023-05-08 20:14:30 -0300

    Alterações dos metodos existentes, e criação de novos metodos na Classe TipoController.

--- src/main/java/br/com/uniamerica/apsystem20/controller/TipoController.java
+++ src/main/java/br/com/uniamerica/apsystem20/controller/TipoController.java
@@ -1,12 +1,15 @@
 package br.com.uniamerica.apsystem20.controller;
 
+import br.com.uniamerica.apsystem20.entity.Produto;
+import br.com.uniamerica.apsystem20.entity.Tipo;
 import br.com.uniamerica.apsystem20.repository.TipoRepository;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/tipo")
@@ -15,10 +18,9 @@ public class TipoController {
     TipoRepository tipoRepository;
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable Long id){
-        return ResponseEntity.ok().body(this.tipoRepository.findById(id));
+    public ResponseEntity<Tipo> findById(@PathVariable Long id){
+        return ResponseEntity.ok().body(this.tipoRepository.findById(id).orElse(new Tipo()));
     }
-
     @GetMapping("/{nomeTipo}")
     public ResponseEntity<?> findByNomeTipo(@PathVariable String nomeTipo){
         return ResponseEntity.ok().body(this.tipoRepository.findByNomeTipo(nomeTipo));
@@ -26,6 +28,36 @@ public class TipoController {
 
     @GetMapping("/{ativo}")
     public ResponseEntity<?> findByAtivo(@PathVariable boolean ativo){
-        return ResponseEntity.ok().body(this.tipoRepository.findByAtivo(ativo));
+        List<Tipo> tipos = this.tipoRepository.findByAtivo(ativo);
+
+        if (tipos.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok().body(tipos);
+    }
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        List<Tipo> tipos = this.tipoRepository.findAll();
+
+        if (tipos.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok().body(tipos);
+    }
+    @PostMapping
+    public ResponseEntity<?> cadastrar(@RequestBody Tipo tipo {
+        this.tipoRepository.save(tipo);
+        return ResponseEntity.ok().body("Registro cadastrado com sucesso");
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> atualizar(@PathVariable final @NotNull Long id, @RequestBody final Tipo tipo) {
+        if (id.equals(tipo.getId()) && !this.tipoRepository.findById(id).isEmpty()) {
+            this.tipoRepository.save(tipo);
+        } else {
+            return ResponseEntity.badRequest().body("Id nao foi encontrado");
+        }
+        return ResponseEntity.ok().body("Registro atualizado com sucesso");
     }
 }
