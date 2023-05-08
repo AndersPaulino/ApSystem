ab2380440f372f2d1125447ac905e265453a8a8a
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2023-05-08 19:59:34 -0300

    Alterações dos metodos existentes, e criação de novos metodos na Classe FornecedorController.

--- src/main/java/br/com/uniamerica/apsystem20/controller/FornecedorController.java
+++ src/main/java/br/com/uniamerica/apsystem20/controller/FornecedorController.java
@@ -1,12 +1,15 @@
 package br.com.uniamerica.apsystem20.controller;
 
+import br.com.uniamerica.apsystem20.entity.Estoque;
+import br.com.uniamerica.apsystem20.entity.Fornecedor;
 import br.com.uniamerica.apsystem20.repository.FornecedorRepository;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/fornecedor")
@@ -14,18 +17,48 @@ public class FornecedorController {
     @Autowired
     FornecedorRepository fornecedorRepository;
 
+
     @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable Long id){
-        return ResponseEntity.ok().body(this.fornecedorRepository.findById(id));
+    public ResponseEntity<Fornecedor> findById(@PathVariable Long id){
+        return ResponseEntity.ok().body(this.fornecedorRepository.findById(id).orElse(new Fornecedor()));
     }
 
     @GetMapping("/{nomeFornecedor}")
     public ResponseEntity<?> findByNomeFornecedor(@PathVariable String nomeFornecedor){
         return ResponseEntity.ok().body(this.fornecedorRepository.findByNomeFornecedor(nomeFornecedor));
     }
-
     @GetMapping("/{ativo}")
     public ResponseEntity<?> findByAtivo(@PathVariable boolean ativo){
-        return ResponseEntity.ok().body(this.fornecedorRepository.findByAtivo(ativo));
+        List<Fornecedor> fornecedores = this.fornecedorRepository.findByAtivo(ativo);
+
+        if (fornecedores.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok().body(fornecedores);
+    }
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        List<Fornecedor> fornecedores = this.fornecedorRepository.findAll();
+
+        if (fornecedores.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok().body(fornecedores);
+    }
+    @PostMapping
+    public ResponseEntity<?> cadastrar(@RequestBody Fornecedor fornecedor) {
+        this.fornecedorRepository.save(fornecedor);
+        return ResponseEntity.ok().body("Registro cadastrado com sucesso");
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> atualizar(@PathVariable final @NotNull Long id, @RequestBody final Fornecedor fornecedor) {
+        if (id.equals(fornecedor.getId()) && !this.fornecedorRepository.findById(id).isEmpty()) {
+            this.fornecedorRepository.save(fornecedor);
+        } else {
+            return ResponseEntity.badRequest().body("Id nao foi encontrado");
+        }
+        return ResponseEntity.ok().body("Registro atualizado com sucesso");
     }
 }
