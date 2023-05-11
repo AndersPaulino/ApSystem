2b7ab5d98b2b744392b166af4eb150756ad059cd
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2023-05-11 15:32:54 -0300

    Correções e atualização da classe FornecedorController, e FornecedorService, implementação de metodos e validações da Service.

--- src/main/java/br/com/uniamerica/apsystem20/controller/FornecedorController.java
+++ src/main/java/br/com/uniamerica/apsystem20/controller/FornecedorController.java
@@ -1,8 +1,8 @@
 package br.com.uniamerica.apsystem20.controller;
 
 import br.com.uniamerica.apsystem20.entity.Fornecedor;
-import br.com.uniamerica.apsystem20.entity.Produto;
 import br.com.uniamerica.apsystem20.repository.FornecedorRepository;
+import br.com.uniamerica.apsystem20.service.FornecedorService;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,33 +10,34 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/fornecedor")
 public class FornecedorController {
+
+    private final FornecedorService fornecedorService;
     private final FornecedorRepository fornecedorRepository;
 
     @Autowired
-    public FornecedorController(FornecedorRepository fornecedorRepository) {
+    public FornecedorController(FornecedorService fornecedorService, FornecedorRepository fornecedorRepository) {
+        this.fornecedorService = fornecedorService;
         this.fornecedorRepository = fornecedorRepository;
     }
-
-
     @GetMapping("/{id}")
     public ResponseEntity<Fornecedor> findById(@PathVariable Long id) {
-        Optional<Fornecedor> fornecedor = fornecedorRepository.findById(id);
+        Optional<Fornecedor> fornecedor = fornecedorService.findById(id);
         if (fornecedor.isPresent()) {
             return ResponseEntity.ok().body(fornecedor.get());
         } else {
             return ResponseEntity.notFound().build();
         }
     }
-
     @GetMapping("/nome/{nomeFornecedor}")
     public ResponseEntity<?> findByNomeFornecedor(@PathVariable String nomeFornecedor) {
-        List<Fornecedor> fornecedors = fornecedorRepository.findByNomeFornecedor(nomeFornecedor);
+        List<Fornecedor> fornecedors = fornecedorService.findByNomeFornecedor(nomeFornecedor);
         if (fornecedors.isEmpty()) {
             return ResponseEntity.noContent().build();
         } else {
@@ -45,7 +46,7 @@ public class FornecedorController {
     }
     @GetMapping("/ativo/{ativo}")
     public ResponseEntity<?> findByAtivo(@PathVariable boolean ativo) {
-        List<Fornecedor> fornecedors = fornecedorRepository.findByAtivo(ativo);
+        List<Fornecedor> fornecedors = fornecedorService.findByAtivo(ativo);
         if (fornecedors.isEmpty()) {
             return ResponseEntity.noContent().build();
         } else {
@@ -54,7 +55,7 @@ public class FornecedorController {
     }
     @GetMapping
     public ResponseEntity<?> findAll() {
-        List<Fornecedor> fornecedors = fornecedorRepository.findAll();
+        List<Fornecedor> fornecedors = fornecedorService.findAll();
         if (fornecedors.isEmpty()) {
             return ResponseEntity.noContent().build();
         } else {
@@ -62,28 +63,23 @@ public class FornecedorController {
         }
     }
     @PostMapping
-    public ResponseEntity<?> cadastrar(@RequestBody final Fornecedor fornecedor) {
-        this.fornecedorRepository.save(fornecedor);
-        return ResponseEntity.ok().body("Registro cadastrado com sucesso");
+    public ResponseEntity<?> cadastrar(@RequestBody Fornecedor fornecedor) {
+        try {
+            fornecedorService.cadastrarFornecedor(fornecedor);
+            return ResponseEntity.ok().body("Registro cadastrado com sucesso!");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
     @PutMapping("/{id}")
-    public ResponseEntity<?> atualizar(@PathVariable final @NotNull Long id, @RequestBody final Fornecedor fornecedor) {
-        Optional<Fornecedor> fornecedorExistente = fornecedorRepository.findById(id);
-
-        if (fornecedorExistente.isPresent()) {
-            Fornecedor fornecedorAtualizado = fornecedorExistente.get();
-            fornecedorAtualizado.setNomeFornecedor(fornecedor.getNomeFornecedor());
-            fornecedorAtualizado.setTelefoneFornecedor(fornecedor.getTelefoneFornecedor());
-            fornecedorAtualizado.setEmail(fornecedor.getEmail());
-
-            try {
-                fornecedorRepository.save(fornecedorAtualizado);
-                return ResponseEntity.ok().body("Registro atualizado com sucesso!");
-            } catch (Exception e) {
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao atualizar o registro.");
-            }
-        } else {
-            return ResponseEntity.badRequest().body("Id inválido!");
+    public ResponseEntity<?> atualizar(@PathVariable @NotNull Long id, @RequestBody Fornecedor fornecedor) {
+        try {
+            fornecedorService.atualizarFornecedor(id, fornecedor);
+            return ResponseEntity.ok().body("Registro atualizado com sucesso!");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao atualizar o registro.");
         }
     }
 }
--- src/main/java/br/com/uniamerica/apsystem20/service/FornecedorService.java
+++ src/main/java/br/com/uniamerica/apsystem20/service/FornecedorService.java
@@ -3,45 +3,67 @@ package br.com.uniamerica.apsystem20.service;
 import br.com.uniamerica.apsystem20.entity.Fornecedor;
 import br.com.uniamerica.apsystem20.repository.FornecedorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class FornecedorService {
-
     @Autowired
     private FornecedorRepository fornecedorRepository;
+    public void validarFornecedor(final Fornecedor fornecedor) {
+        Assert.notNull(fornecedor.getNomeFornecedor(), "Nome do fornecedor não informado");
+        Assert.notNull(fornecedor.getTelefoneFornecedor(), "Telefone do fornecedor não informado");
 
-    public void validarFornecedor(final Fornecedor fornecedor){
+        if (fornecedor.getEmail().isEmpty()) {
+            throw new IllegalArgumentException("E-mail do fornecedor não informado");
+        }
 
-        Assert.isTrue(fornecedor.getNomeFornecedor() != null,
-                "Nome do fornecedor nao informado");
+        if (!fornecedor.getEmail().contains("@")) {
+            throw new IllegalArgumentException("E-mail do fornecedor inválido");
+        }
+    }
+    public Optional<Fornecedor> findById(Long id) {
+        return fornecedorRepository.findById(id);
+    }
 
-        Assert.isTrue(fornecedor.getTelefoneFornecedor() != null,
-                "Telefone do fornecedor não informado");
+    public List<Fornecedor> findByNomeFornecedor(String nomeFornecedor) {
+        return fornecedorRepository.findByNomeFornecedor(nomeFornecedor);
+    }
 
-        Assert.isTrue(fornecedor.getEmail() != null,
-                "E-mail do fornecedor não informado");
+    public List<Fornecedor> findByAtivo(boolean ativo) {
+        return fornecedorRepository.findByAtivo(ativo);
+    }
 
+    public List<Fornecedor> findAll() {
+        return fornecedorRepository.findAll();
     }
 
     @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRES_NEW)
-    public void cadastrar(final Fornecedor fornecedor){
-        this.validarFornecedor(fornecedor);
-        this.fornecedorRepository.save(fornecedor);
+    public void cadastrarFornecedor(Fornecedor fornecedor) {
+        validarFornecedor(fornecedor);
+        fornecedorRepository.save(fornecedor);
     }
-
     @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRES_NEW)
-    public void atualizar(final Long id, final Fornecedor fornecedor){
+    public void atualizarFornecedor(Long id, Fornecedor fornecedor) {
+        validarFornecedor(fornecedor);
+        Optional<Fornecedor> fornecedorExistente = fornecedorRepository.findById(id);
 
-        if(id.equals(fornecedor.getId()) && !this.fornecedorRepository.findById(id).isEmpty()){
-            this.validarFornecedor(fornecedor);
-            this.fornecedorRepository.save(fornecedor);
-        }
-        else{
-            throw new RuntimeException("Id nao encontrado.");
+        if (fornecedorExistente.isPresent()) {
+            Fornecedor fornecedorAtualizado = fornecedorExistente.get();
+            fornecedorAtualizado.setNomeFornecedor(fornecedor.getNomeFornecedor());
+            fornecedorAtualizado.setTelefoneFornecedor(fornecedor.getTelefoneFornecedor());
+            fornecedorAtualizado.setEmail(fornecedor.getEmail());
+
+            fornecedorRepository.save(fornecedorAtualizado);
+        } else {
+            throw new IllegalArgumentException("Id inválido!");
         }
     }
+
 }
